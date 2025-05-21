9ad1a48561b253cb4102c38770f4b93c5c04603d
jugne <ugne.stolz@bsse.ethz.ch>
2025-05-21 21:19:36 +0200

    Adds sigmoid shape and midpoint.

--- BayesMLP/src/bayesmlp/Sigmoid.java
+++ BayesMLP/src/bayesmlp/Sigmoid.java
@@ -11,8 +11,22 @@ public class Sigmoid extends ActivationFunction {
     public Input<Double> upperInput = new Input<>("upper",
             "Upper bound for sigmoid scaling", 1.0);
 
+    public Input<Double> sInput = new Input<>("shape",
+            "Shape (steepness). Larger values mean transition is steeper.", 1.0);
+
+    public Input<Double> midInput = new Input<>("midpoint",
+            "Midpoint: the value of input at which the output is halfway between lower and upper",
+            0.0);
+
+
+
+
+
     private double lower;
     private double upper;
+
+    private double s; //shape (steepness)
+    private double mid; // midpoint: the value of z at which the output is halfway between lower and upper
 
 
     @Override
@@ -22,6 +36,7 @@ public class Sigmoid extends ActivationFunction {
         if (lower >= upper){
             throw new IllegalArgumentException("Lower bound should not be less than upper bound.");
         }
+
     }
 
 
@@ -31,7 +46,7 @@ public class Sigmoid extends ActivationFunction {
         for (int i = 0; i < z.getRowDimension(); i++) {
             for (int j = 0; j < z.getColumnDimension(); j++) {
                 double raw = z.getEntry(i, j);
-                double scaled = lower + (upper - lower) / (1 + Math.exp(-raw));
+                double scaled = lower + (upper - lower) / (1 + Math.exp(-s * (raw - mid)));
                 result.setEntry(i, j, scaled);
             }
         }
